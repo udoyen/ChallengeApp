73a5f9cca6eb72fcde70b565faa79ce95a1652fe
udoyen <datameshprojects@gmail.com>
2019-07-11 20:39:01 +0100

    initial commit

---
+++ app/src/main/java/com/connect/systems/ng/challengeapp/AboutAndelaActivity.kt
@@ -0,0 +1,12 @@
+package com.connect.systems.ng.challengeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutAndelaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_andela)
+    }
+}
---
+++ app/src/main/java/com/connect/systems/ng/challengeapp/ProfileActivity.kt
@@ -0,0 +1,19 @@
+package com.connect.systems.ng.challengeapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_profile.*
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+        setSupportActionBar(toolbar)
+
+
+    }
+
+}
