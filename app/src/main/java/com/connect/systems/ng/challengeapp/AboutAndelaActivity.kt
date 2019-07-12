e5b6201f885bf88c30ee9eb83be39e5361b4d3fd
udoyen <datameshprojects@gmail.com>
2019-07-12 15:58:28 +0100

    webview display done

--- app/src/main/java/com/connect/systems/ng/challengeapp/AboutAndelaActivity.kt
+++ app/src/main/java/com/connect/systems/ng/challengeapp/AboutAndelaActivity.kt
@@ -1,12 +1,30 @@
 package com.connect.systems.ng.challengeapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class AboutAndelaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_andela)
+//        setSupportActionBar(toolbar)
+        val andelaWebView = findViewById<WebView>(R.id.andelaWebView)
+        val webviewtxt = findViewById<TextView>(R.id.webViewTxt)
+        // Get the url
+        val extras = intent.extras  ?: return
+        val urlString = extras.getString("andelaURL")
+        // set the webclient
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            andelaWebView.webViewClient = WebViewClient()
+        }
+        webviewtxt.setText(urlString)
+        // Load the URL
+        andelaWebView.loadUrl(urlString)
     }
 }
--- app/src/main/java/com/connect/systems/ng/challengeapp/MainActivity.kt
+++ app/src/main/java/com/connect/systems/ng/challengeapp/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.connect.systems.ng.challengeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val andelaBtn = findViewById<Button>(R.id.activityABtn)
+        val myUrlBtn = findViewById<Button>(R.id.myProfileBtn)
+
+        //set the onclick event listener
+        andelaBtn.setOnClickListener {
+            val andelaIntent = Intent(this, AboutAndelaActivity::class.java)
+            andelaIntent.putExtra("andelaURL", "https://google.com/")
+            startActivity(andelaIntent)
+        }
+
+        myUrlBtn.setOnClickListener {
+            val myProfileIntent = Intent(this, ProfileActivity::class.java)
+            startActivity(myProfileIntent)
+        }
     }
+
+
 }
